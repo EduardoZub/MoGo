5473846b4d269ff6ef76ed7876b49e3d8144b1ee
EduardoZub <deveduardzubal@gmail.com>
2018-10-18 22:56:25 +0300

    MoGo commit

---
+++ src/js/main.js
@@ -0,0 +1,46 @@
+"use strict";
+$(document).ready(function() {
+    // script for search field START
+    $(".icon-header__search").click(function() {
+        $("body").toggleClass("show-search-field");
+    });
+
+    // script for section-what-we-do slider START
+    $(".slider").slick({
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+            // autoplay: true,
+            // autoplaySpeed: 2000
+    });
+    // $("#show-slide1").on('click', function() {
+    //     // slideIndex++;
+    // });
+    // script for section-coments slider START
+    $(".section-coments-slider").slick({
+        infinite: true,
+        speed: 500
+    });
+
+    // script for section-what-we-do accordion START
+    // $(function() {
+    $("#accordion").accordion({
+        heightStyle: "auto",
+        icons: {
+            "header": "icon-active ",
+            "activeHeader": "icon-not-active"
+        }
+    });
+
+    $("#accordion-resizer").resizable({
+        minHeight: 0,
+        minWidth: 600,
+        maxHeight: 200,
+        maxWidth: 600,
+        resize: function() {
+            $("#accordion").accordion("refresh");
+        }
+    });
+    // });
+});
